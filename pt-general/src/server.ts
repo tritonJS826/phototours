db9e0e2d5d8165dcc6eca8b8087da2e1facd2fc3
tritonJS826 <triton.68@mail.ru>
2025-07-28 15:47:31 +0200

    feat: #7 integrated back with postgres with prisma, added example endpoint

--- pt-general/src/server.ts
+++ pt-general/src/server.ts
@@ -1,11 +1,31 @@
 import {env} from 'src/config/env';
+import {prisma} from 'src/db/prisma';
 import express, {Express, Request, Response} from 'express';
 
+const CODE_500 = 500;
+
 const app: Express = express();
+app.use(express.json());
 const port = env.SERVER_PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
+});
+
+app.post('/users', async (req: Request, res: Response) => {
+  try {
+    const {email, name} = req.body;
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        password: 'test',
+      },
+    });
+    res.json(user);
+  } catch (error) {
+    res.status(CODE_500).json({error});
+  }
 });
 
 app.listen(port, () => {
