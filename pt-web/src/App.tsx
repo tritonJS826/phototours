7a916b5750bca8be9dc6591fc22c20ae7455b4ca
Valentin <valentingerner13@gmail.com>
2025-07-29 10:03:02 +0200

    feat: #9 added Link component with support for RouterLink and external links

--- pt-web/src/App.tsx
+++ pt-web/src/App.tsx
@@ -1,9 +1,13 @@
 import {HelloButton} from "src/components/ButtonHello/ButtonHello";
+import {Link} from "src/components/ui/link/Link";
 
 export function App() {
   return (
     <div>
       <HelloButton />
+      <Link href="https://www.google.com">
+        Go to google
+      </Link>
     </div>
   );
 }
