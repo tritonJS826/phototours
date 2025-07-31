65484dfd26b64b5897d0727e2bb81450af4720ad
nataliia-tyrsa <nataliiatyrsa@gmail.com>
2025-07-31 15:14:24 +0200

    feat: #11 create all page stubs (#20)

---
+++ pt-web/src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import {Container} from "src/components/Container/Container";
+
+export function Header() {
+  return (
+    <Container>
+      <p>
+        Остальной контент
+      </p>
+    </Container>
+  );
+}
---
+++ pt-web/src/pages/profil/index.tsx
@@ -0,0 +1,11 @@
+<div>
+  <h1>
+    404 - Page Not Found
+  </h1>
+  <p>
+    The page you are looking for does not exist.
+  </p>
+  <a href="/">
+    Go back to Home
+  </a>
+</div>;
