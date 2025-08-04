fdb2ad6d079b675870dcc040c4c822389e21d4ed
valger-lab <valentingerner13@gmail.com>
2025-08-04 17:04:35 +0200

    feat: #45 Cloudinary settings (#56)
    * feat: #45 Cloudinary settings added
    
    * refactor: #45 Cleaned up non-working code
    
    * docs: #45 Added Cloudinary setup instructions
    
    * fix: #45 resolved minor issues found during review

--- pt-general/src/routes/tourRoutes.ts
+++ pt-general/src/routes/tourRoutes.ts
@@ -11,6 +11,7 @@ import {
   updateTourCategories,
   updateTourTags,
 } from 'src/controllers/tourControllers';
+import {upload} from 'src/middleware/upload';
 import {Router} from 'express';
 
 export const tourRoutes = Router();
@@ -24,6 +25,6 @@ tourRoutes.delete('/:id', deleteTour);
 tourRoutes.patch('/:id/categories', updateTourCategories);
 tourRoutes.patch('/:id/tags', updateTourTags);
 tourRoutes.patch('/:id/dates', addTourDates);
-tourRoutes.patch('/:id/photos', addTourPhotos);
-tourRoutes.patch('/:id/videos', addTourVideos);
+tourRoutes.patch('/:id/photos', upload.single('file'), addTourPhotos);
+tourRoutes.patch('/:id/videos', upload.single('file'), addTourVideos);
 tourRoutes.patch('/:id/materials', addTourMaterials);
