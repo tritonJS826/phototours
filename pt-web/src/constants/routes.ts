c4255ff9c14016814f680e8608eb4ba9d7aa9723
nataliia-tyrsa <nataliiatyrsa@gmail.com>
2025-08-06 02:28:37 +0200

    feat: #18 refactor Header component and  styles

---
+++ pt-web/src/constants/routes.ts
@@ -0,0 +1,12 @@
+export const PATHS = {
+  HOME: "/",
+  ABOUT: "/about",
+  ADMIN: "/admin",
+  TOURS: "/tours",
+  TOUR_DETAILS: "/tours/:id",
+  CART: "/cart",
+  CONTACT: "/contact",
+  ARTICLES: "/articles",
+  PROFILE: "/profile",
+  NOT_FOUND: "*",
+};
--- pt-web/src/pages/homePage/index.tsx
+++ pt-web/src/pages/homePage/index.tsx
@@ -1,20 +1,14 @@
 import {Container} from "src/components/Container/Container";
-import {Footer} from "src/components/Footer/Footer";
-import {Header} from "src/components/Header/Header";
 import styles from "src/pages/homePage/HomePage.module.scss";
 
 export function HomePage() {
   return (
-    <>
-      <Header />
-      <Container>
-        <div className={styles.content}>
-          <h1>
-            Главная страница
-          </h1>
-        </div>
-      </Container>
-      <Footer />
-    </>
+    <Container>
+      <div className={styles.content}>
+        <h1>
+          Главная страница
+        </h1>
+      </div>
+    </Container>
   );
 }
