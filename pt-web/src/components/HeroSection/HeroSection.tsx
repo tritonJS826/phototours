5361465cb130b928ebcb452f87c94571d25932fd
nataliia-tyrsa <nataliiatyrsa@gmail.com>
2025-08-08 23:58:56 +0200

    Pt 24 create homepage (#74)
    * feat: #24 create component HeroSection
    
    * feat: #24 changed Header
    
    * feat: #24 create HeroSection
    
    * feat: #24 changed size background

---
+++ pt-web/src/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,33 @@
+import {Container} from "src/components/Container/Container";
+import styles from "src/components/HeroSection/HeroSection.module.scss";
+
+export function HeroSection() {
+  return (
+    <Container>
+      <section className={styles.heroSection}>
+        <div className={styles.heroContent}>
+          <h1 className={styles.heroTitle}>
+            <span>
+              Everything you need
+            </span>
+            <span>
+              for your dream photography
+            </span>
+            <span>
+              adventure in Iceland
+            </span>
+          </h1>
+          <p className={styles.heroDescription}>
+            <span>
+              As Iceland's leading photo tour operator, we offer expert tours/
+            </span>
+            <span>
+              to help you capture breathtaking images at the best spots-guaranteed
+            </span>
+          </p>
+        </div>
+      </section>
+    </Container>
+  );
+}
+
