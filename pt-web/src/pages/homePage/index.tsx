b29b248bead6517ca0f7c2c94543f51debb4c3e7
nataliia-tyrsa <nataliiatyrsa@gmail.com>
2025-08-08 23:22:46 +0200

    feat: #24 changed Header

--- pt-web/src/pages/homePage/index.tsx
+++ pt-web/src/pages/homePage/index.tsx
@@ -1,14 +1,10 @@
 import {HeroSection} from "src/components/HeroSection/HeroSection";
 import styles from "src/pages/homePage/HomePage.module.scss";
-// Import {ArticlesCarousel} from "src/features/articles/ArticlesCarousel";
 
 export function HomePage() {
   return (
-    <>
-      <div className={styles.homePage}>
-        <HeroSection />
-        {/* <ArticlesCarousel /> */}
-      </div>
-    </>
+    <div className={styles.homePage}>
+      <HeroSection />
+    </div>
   );
 }
