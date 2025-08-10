e087c21191872b5a1ddcdde4ef91bf00dbd405ad
nataliia-tyrsa <nataliiatyrsa@gmail.com>
2025-08-10 15:48:45 +0200

    feat: #28 create ExploreArticles page

--- pt-web/src/pages/exploreArticles/index.tsx
+++ pt-web/src/pages/exploreArticles/index.tsx
@@ -1,11 +1,16 @@
+import {FirstArticle} from "src/components/Articles/FirstArticle";
+import {FourthArticle} from "src/components/Articles/FourthArticle";
+import {SecondArticle} from "src/components/Articles/SecondArticle";
+import {ThirdArticle} from "src/components/Articles/ThirdArticle";
 import {Container} from "src/components/Container/Container";
 
 export function ExploreArticles() {
   return (
     <Container>
-      <p>
-        Остальной контент
-      </p>
+      <FirstArticle />
+      <SecondArticle />
+      <ThirdArticle />
+      <FourthArticle />
     </Container>
   );
 }
