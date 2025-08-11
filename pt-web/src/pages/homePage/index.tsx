9a16055ec58a0ca9f368090d65f650cad1a2fabc
nataliia-tyrsa <nataliiatyrsa@gmail.com>
2025-08-11 15:44:22 +0200

    feat: #24 home page prototype

--- pt-web/src/pages/homePage/index.tsx
+++ pt-web/src/pages/homePage/index.tsx
@@ -1,6 +1,8 @@
+import {BestTravelTips} from "src/components/BestTravelTips/BestTravelTips";
 import {HeroSection} from "src/components/HeroSection/HeroSection";
 import {PopularWorkshops} from "src/components/PopularWorkshops/PopularWorkshops";
 import {TourSearchForm} from "src/components/TourSearchForm/TourSearchForm";
+import {articles} from "src/features/articles/articles.data";
 import styles from "src/pages/homePage/HomePage.module.scss";
 
 export function HomePage() {
@@ -9,6 +11,7 @@ export function HomePage() {
       <HeroSection />
       <TourSearchForm />
       <PopularWorkshops />
+      <BestTravelTips a={articles[0]} />
     </div>
   );
 }
