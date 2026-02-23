6f5ed3a3e6dd9544c39529531d940975d0ff8205
tritonJS826 <triton.68@mail.ru>
2026-02-23 06:29:16 +0100

    updated image with better resolution on main page

--- pt-web/src/components/PopularDestinations/PopularDestinations.tsx
+++ pt-web/src/components/PopularDestinations/PopularDestinations.tsx
@@ -10,37 +10,37 @@ type PopularWorkshopsProps = {
 const slides = [
   {
     image:
-    "https://res.cloudinary.com/dxqcrv4gf/image/upload/v1771392145/mainPageTop1TuscanyFall_iqyoek.avif",
+    "https://res.cloudinary.com/dxqcrv4gf/image/upload/v1771824429/mainPageTop1TuscanyFall_dzsr1a.avif",
     title: "Misty Tuscan Harvest",
     href: PATHS.getTour("tuscany-autumn-tour"),
   },
   {
     image:
-     "https://res.cloudinary.com/dxqcrv4gf/image/upload/v1771392146/mainPageTop2Iceland_wakqlk.avif",
+     "https://res.cloudinary.com/dxqcrv4gf/image/upload/v1771824430/mainPageTop2Iceland_miysef.avif",
     title: "Iceland Off-Road Adventure",
     href: PATHS.getTour("iceland-tour"),
   },
   {
     image:
-     "https://res.cloudinary.com/dxqcrv4gf/image/upload/v1771392148/mainPageTop3NewZealand_qvbblq.avif",
+     "https://res.cloudinary.com/dxqcrv4gf/image/upload/v1771824430/mainPageTop3NewZealand_ekuw7r.avif",
     title: "Epic New Zealand",
     href: PATHS.getTour("new-zealand-photo-tour"),
   },
   {
     image:
-     "https://res.cloudinary.com/dxqcrv4gf/image/upload/v1771392149/mainPageTop4Venice_wskajb.avif",
+     "https://res.cloudinary.com/dxqcrv4gf/image/upload/v1771824431/mainPageTop4Venice_on23ox.avif",
     title: "Mystical Venice Masquerade",
     href: PATHS.getTour("venice-carnival-photo-tour"),
   },
   {
     image:
-     "https://res.cloudinary.com/dxqcrv4gf/image/upload/v1771392150/mainPageTop5Czech_mha71k.avif",
+     "https://res.cloudinary.com/dxqcrv4gf/image/upload/v1771824431/mainPageTop5CzechFall_h6iohn.avif",
     title: "Golden Prague & Moravia",
     href: PATHS.getTour("czechia-autumn-tour"),
   },
   {
     image:
-     "https://res.cloudinary.com/dxqcrv4gf/image/upload/v1771392152/mainPageTop6TuscanySpring_gfaqm9.avif",
+     "https://res.cloudinary.com/dxqcrv4gf/image/upload/v1771824431/mainPageTop5CzechFall_h6iohn.avif",
     title: "Dreamy\nTuscan Fog",
     href: PATHS.getTour("tuscany-spring-photo-tour"),
   },
