a6b81af5c63b0193632522c71546576d67048009
tritonJS826 <triton.68@mail.ru>
2026-03-01 08:30:57 +0100

    fixed horizontal scroll articles page

--- pt-web/src/pages/exploreArticles/ArticlesPage/ArticlesPage.tsx
+++ pt-web/src/pages/exploreArticles/ArticlesPage/ArticlesPage.tsx
@@ -19,23 +19,21 @@ export function ArticlesPage() {
 
   return (
     <main className={styles.page}>
-      <div className="container">
-        <h1 className={styles.title}>
-          Blog & Photography Guides
-        </h1>
+      <h1 className={styles.title}>
+        Blog & Photography Guides
+      </h1>
 
-        <h2 className={styles.subTitle}>
-          Expert insights, practical tips, and stories designed to elevate your photography and uncover the world's hidden gems.
-        </h2>
-        <section className={styles.list}>
-          {items.map((a) => (
-            <ArticleCard
-              key={a.id}
-              item={a}
-            />
-          ))}
-        </section>
-      </div>
+      <h2 className={styles.subTitle}>
+        Expert insights, practical tips, and stories designed to elevate your photography and uncover the world's hidden gems.
+      </h2>
+      <section className={styles.list}>
+        {items.map((a) => (
+          <ArticleCard
+            key={a.id}
+            item={a}
+          />
+        ))}
+      </section>
     </main>
   );
 }
